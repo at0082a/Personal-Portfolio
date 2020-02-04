428bb403f1fdc9e9009e59fdce7975aa3e34adcd
Alex Tilatti <alex.tilatti@gmail.com>
2020-02-03 21:06:56 -0500

    added all page components with styling

--- src/pages/Projects/styles.js
+++ src/pages/Projects/styles.js
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
 import { white } from '@carbon/colors';
 
-export const EducationItem = styled.li`
+export const ProjectItem = styled.li`
   margin-top: 1rem;
   padding-bottom: 1rem;
   border-bottom: 1px solid ${white};
 `;
 
-export const Institution = styled.h4`
+export const ProjectTitle = styled.h4`
   font-weight: bold;
 `;
 
-export const Degree = styled.p`
-  font-weight: bold;
-  display: inline-block;
+export const SkillContainer = styled.div`
+  margin-top: 1.2rem; 
 `;
