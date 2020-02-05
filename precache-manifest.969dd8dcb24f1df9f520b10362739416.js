53856f3a39f352d7c0ed81f9f6d5b7aa02f933be
Alex Tilatti <alex.tilatti@gmail.com>
2020-02-04 23:08:08 -0500

    Updates

---
+++ precache-manifest.969dd8dcb24f1df9f520b10362739416.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68958c0abcf2c1e8fcdbf550a4a5e00",
+    "url": "/Personal-Portfolio/index.html"
+  },
+  {
+    "revision": "1f0a7cda70c44201cb5d",
+    "url": "/Personal-Portfolio/static/css/main.08785a31.chunk.css"
+  },
+  {
+    "revision": "d97deb2043d0098628e4",
+    "url": "/Personal-Portfolio/static/js/2.ccc1de6a.chunk.js"
+  },
+  {
+    "revision": "bc0c83fb5c6c8b946b94c2ec61dd7b90",
+    "url": "/Personal-Portfolio/static/js/2.ccc1de6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f0a7cda70c44201cb5d",
+    "url": "/Personal-Portfolio/static/js/main.047069ab.chunk.js"
+  },
+  {
+    "revision": "ea248a1d1f08b7b7dca6",
+    "url": "/Personal-Portfolio/static/js/runtime-main.7fdfc1ce.js"
+  },
+  {
+    "revision": "79e1f3e3df9ca09d115b2bf61bda7baf",
+    "url": "/Personal-Portfolio/static/media/AlexTAvatar.79e1f3e3.jpg"
+  }
+]);
